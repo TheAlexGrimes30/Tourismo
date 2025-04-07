f95958b7d1da976dd7496bd01de959ed4bf88b52
alexborodach30 <sagopien04@gmail.com>
2025-04-07 14:05:47 +0300

    Preview Delay.

--- app/src/main/java/com/example/turismo/PreviewActivity.kt
+++ app/src/main/java/com/example/turismo/PreviewActivity.kt
@@ -1,20 +1,34 @@
 package com.example.turismo
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class PreviewActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_preview)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        MainScope().launch {
+            delay(2000)
+            val intent = Intent(this@PreviewActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
