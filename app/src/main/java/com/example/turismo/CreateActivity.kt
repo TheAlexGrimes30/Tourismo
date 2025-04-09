c5f9809ee94cfb2679440fe5b5147db355bd484b
alexborodach30 <sagopien04@gmail.com>
2025-04-09 13:20:25 +0300

    Gallery connected.

--- app/src/main/java/com/example/turismo/CreateActivity.kt
+++ app/src/main/java/com/example/turismo/CreateActivity.kt
@@ -1,20 +1,50 @@
 package com.example.turismo
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class CreateActivity : ComponentActivity() {
+
+    private lateinit var addFileIcon: ImageView
+    private var imageUri: Uri? = null
+
+    private val pickImageLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val data = result.data
+            imageUri = data?.data
+            imageUri?.let {
+                addFileIcon.setImageURI(it)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_create)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        addFileIcon = findViewById(R.id.addFileIcon)
+        addFileIcon.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_PICK).apply {
+                type = "image/*"
+            }
+            pickImageLauncher.launch(intent)
         }
     }
 }
