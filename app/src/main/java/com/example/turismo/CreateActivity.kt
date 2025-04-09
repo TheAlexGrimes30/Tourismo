87457a18bd1a895a559a2972e1140df27a9cd0ff
alexborodach30 <sagopien04@gmail.com>
2025-04-09 13:10:41 +0300

    Created Add page transition.

--- app/src/main/java/com/example/turismo/CreateActivity.kt
+++ app/src/main/java/com/example/turismo/CreateActivity.kt
@@ -1,12 +1,12 @@
 package com.example.turismo
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class CreateActivity : AppCompatActivity() {
+class CreateActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- app/src/main/java/com/example/turismo/MainActivity.kt
+++ app/src/main/java/com/example/turismo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.turismo
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Button
@@ -21,7 +22,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -187,7 +187,7 @@ fun HomeScreen() {
                     LayoutInflater.from(ctx).inflate(R.layout.bottom_sheet_dialog, null).apply {
                         val addButton = findViewById<Button>(R.id.btn_add_location)
                         addButton.setOnClickListener {
-                            Toast.makeText(ctx, "Место добавлено", Toast.LENGTH_SHORT).show()
+                            ctx.startActivity(Intent(ctx, CreateActivity::class.java))
                             coroutineScope.launch {
                                 bottomSheetState.hide()
                             }
